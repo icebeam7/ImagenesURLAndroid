c80d42c58cbc514d5779bcebcd13134df56065d4
icebeam7 <luis.beltran@itcelaya.edu.mx>
2017-06-07 16:54:37 +0200

    Mostrar imagen desde URL en Xamarin Android

--- ImagenesURLAndroid/MainActivity.cs
+++ ImagenesURLAndroid/MainActivity.cs
@@ -1,18 +1,38 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Graphics;
+using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace ImagenesURLAndroid
 {
     [Activity(Label = "ImagenesURLAndroid", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle bundle)
+        protected async override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            var url = "https://pbs.twimg.com/profile_images/861171276756377600/LMwEjMe9.jpg";
+            var bitmap = await DescargarImagenURLAsync(url);
+            var foto = FindViewById<ImageView>(Resource.Id.foto);
+            foto.SetImageBitmap(bitmap);
+        }
+
+        private async Task<Bitmap> DescargarImagenURLAsync(string url)
+        {
+            Bitmap bitmap = null;
+
+            using (var httpClient = new HttpClient())
+            {
+                var bytes = await httpClient.GetByteArrayAsync(url);
+                if (bytes != null && bytes.Length > 0)
+                    bitmap = BitmapFactory.DecodeByteArray(bytes, 0, bytes.Length);
+            }
+            return bitmap;
         }
     }
 }
